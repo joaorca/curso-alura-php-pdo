5786c5271b932f331f17c84b655ca99b165965e2
João R C Almeida <joaorca@unisuam.edu.br>
2020-05-09 22:37:24 -0300

    Modos de busca

--- lista-alunos.php
+++ lista-alunos.php
@@ -1,11 +1,22 @@
 <?php
 
+use Alura\Pdo\Domain\Model\Student;
+
 require_once 'vendor/autoload.php';
 
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO ('sqlite:' . $databasePath);
 
 $statement = $pdo->query('SELECT * FROM students');
-$studentList = $statement->fetchAll();
+$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$studentList = [];
 
-var_dump($studentList);
+foreach ($studentDataList as $stutendData) {
+    $studentList[] = new Student(
+        $stutendData['id'],
+        $stutendData['name'],
+        new DateTimeImmutable($stutendData['birth_date'])
+    );
+}
+
+print_r($studentList);
