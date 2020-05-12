8335fec715b0bcb71cc2b5b7dcd7a24c6d34b1cd
João R C Almeida <joaorca@unisuam.edu.br>
2020-05-12 08:41:50 -0300

    Pegando o erro

--- src/Infrastructure/Repository/PdoStudentRepository.php
+++ src/Infrastructure/Repository/PdoStudentRepository.php
@@ -86,11 +86,11 @@ class PdoStudentRepository implements StudentRepository
      */
     private function insert(Student $student)
     {
-        $insertQuery = 'INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);';
+        $insertQuery = 'INSERT INTO studentsa (name, birth_date) VALUES (:name, :birth_date);';
         $stmt = $this->connection->prepare($insertQuery);
 
         if ($stmt === false){
-            throw new RuntimeException('Erro na query do banco');
+            throw new RuntimeException($this->connection->errorInfo()[2]);
         }
 
         $success = $stmt->execute(
