214e980f145f6a2b9e8e075afa7346389b8b7dcc
João R C Almeida <joaorca@unisuam.edu.br>
2020-05-11 23:44:22 -0300

    Implementação do PdoStudentRepository

--- src/Infrastructure/Repository/PdoStudentRepository.php
+++ src/Infrastructure/Repository/PdoStudentRepository.php
@@ -5,33 +5,129 @@ namespace Alura\Pdo\Infrastructure\Repository;
 use Alura\Pdo\Domain\Model\Student;
 use Alura\Pdo\Domain\Repository\StudentRepository;
 use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use DateTimeImmutable;
+use Exception;
+use PDO;
+use PDOStatement;
 
 class PdoStudentRepository implements StudentRepository
 {
-    private \PDO $connection;
+    private PDO $connection;
 
     public function __construct()
     {
         $this->connection = ConnectionCreator::createConnection();
     }
 
+    /**
+     * @return array
+     * @throws Exception
+     */
     public function allStudents(): array
     {
-        // TODO: Implement allStudents() method.
+        $sqlQuery = 'SELECT * FROM students;';
+        $stmt = $this->connection->query($sqlQuery);
+
+        return $this->hydrateStudentList($stmt);
     }
 
-    public function studentsBirthAt(\DateTimeImmutable $birthDate): array
+    /**
+     * @param DateTimeImmutable $birthDate
+     * @return array
+     * @throws Exception
+     */
+    public function studentsBirthAt(DateTimeImmutable $birthDate): array
     {
-        // TODO: Implement studentsBirthAt() method.
+        $sqlQuery = 'SELECT * FROM students WHERE birth_date = ?;';
+        $stmt = $this->connection->query($sqlQuery);
+        $stmt->bindValue(1, $birthDate->format('Y-m-d'));
+        $stmt->execute();
+
+        return $this->hydrateStudentList($stmt);
     }
 
+    /**
+     * @param PDOStatement $stmt
+     * @return array
+     * @throws Exception
+     */
+    private function hydrateStudentList(PDOStatement $stmt): array
+    {
+        $studentDataList = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $studentList = [];
+
+        foreach ($studentDataList as $studentData) {
+            $studentList[] = new Student(
+                $studentData['id'],
+                $studentData['name'],
+                new DateTimeImmutable($studentData['birth_date'])
+            );
+        }
+
+        return $studentList;
+    }
+
+    /**
+     * @param Student $student
+     * @return bool
+     */
     public function save(Student $student): bool
     {
-        // TODO: Implement save() method.
+        if ($student->id() === null) {
+            return $this->insert($student);
+        }
+
+        return $this->update($student);
     }
 
+    /**
+     * @param Student $student
+     * @return bool
+     */
+    private function insert(Student $student)
+    {
+        $insertQuery = 'INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);';
+        $stmt = $this->connection->prepare($insertQuery);
+
+        $success = $stmt->execute(
+            [
+                ':name' => $student->name(),
+                ':birth_date' => $student->birthDate()->format('Y-m-d')
+
+            ]
+        );
+
+        if ($success) {
+            $student->defineId($this->connection->lastInsertId());
+        }
+
+        return $success;
+    }
+
+    /**
+     * @param Student $student
+     * @return bool
+     */
+    private function update(Student $student): bool
+    {
+        $updateQuery = 'UPDATE students SET name = :name, birth_date = :birth_date WHERE id = :id;';
+        $stmt = $this->connection->prepare($updateQuery);
+        $stmt->bindValue(':name', $student->name());
+        $stmt->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
+        $stmt->bindValue(':id', $student->id(), PDO::PARAM_INT);
+
+        return $stmt->execute();
+    }
+
+    /**
+     * @param Student $student
+     * @return bool
+     */
     public function remove(Student $student): bool
     {
-        // TODO: Implement remove() method.
+        $stmt = $this->connection->prepare('DELETE FROM students WHERE id = ?');
+        $stmt->bindValue(1, $student->id(), PDO::PARAM_INT);
+
+        return $stmt->execute();
     }
 }
