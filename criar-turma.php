9c8f166555794e3243b763af9e6d781af891b362
João R C Almeida <joaorca@unisuam.edu.br>
2020-05-11 23:52:52 -0300

    Iniciando com transações

---
+++ criar-turma.php
@@ -0,0 +1,28 @@
+<?php
+
+use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
+
+require_once 'vendor/autoload.php';
+
+$connection = ConnectionCreator::createConnection();
+$studentRepository = new PdoStudentRepository($connection);
+
+$connection->beginTransaction();
+
+$aStudent = new Student(
+    null,
+    'Nico Steppat',
+    new DateTimeImmutable('1985-05-01')
+);
+$studentRepository->save($aStudent);
+
+$bStudent = new Student(
+    null,
+    'Sergio Lopes',
+    new DateTimeImmutable('1985-05-01')
+);
+$studentRepository->save($bStudent);
+
+$connection->commit();
