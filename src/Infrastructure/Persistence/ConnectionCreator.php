8baf568e94c8024cd322a9b6e224c43b5021c5e4
João R C Almeida <joaorca@unisuam.edu.br>
2020-05-12 09:01:46 -0300

    Outros parâmetros

--- src/Infrastructure/Persistence/ConnectionCreator.php
+++ src/Infrastructure/Persistence/ConnectionCreator.php
@@ -11,6 +11,7 @@ class ConnectionCreator
         $databasePath = __DIR__ . '/../../../banco.sqlite';
         $pdo = new PDO ('sqlite:' . $databasePath);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         return $pdo;
     }
 }
